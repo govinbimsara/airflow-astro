a26672924571375aa64558396f358418f43e88c8
Govin <govinbimsara@gmail.com>
2024-12-23 09:21:50 +0000

    math_operation.py code correction

--- dags/math_operations.py
+++ dags/math_operations.py
@@ -9,25 +9,25 @@ def start_number(**context):
     print("Starting number is 10")
 
 def add_five(**context):
-    current_value=context['ti'].xcom_pull(key='current_value',task_id='start_task')#
+    current_value=context['ti'].xcom_pull(key='current_value',task_ids='start_task')#
     new_value = current_value +5 
     context['ti'].xcom_push(key='current_value',value=new_value)
     print(f'Add 5: {current_value} + 5 = {new_value}')
 
 def multiply_by_two(**context):
-    current_value=context['ti'].xcom_pull(key='current_value',task_id='add_five_task')
+    current_value=context['ti'].xcom_pull(key='current_value',task_ids='add_five_task')
     new_value = current_value * 2
     context['ti'].xcom_push(key='current_value',value=new_value)
     print(f'Multiply by 2: {current_value} * 2 = {new_value}')
 
 def subtract_three(**context):
-    current_value = context['ti'].xcom_pull(key='current_value',task_id='multiply_by_two_task')
+    current_value = context['ti'].xcom_pull(key='current_value',task_ids='multiply_by_two_task')
     new_value = current_value -3 
     context['ti'].xcom_push(key='current_value',value=new_value)
     print(f'Subtract 3: {current_value} - 3 = {new_value}')
 
 def square_number(**context):
-    current_value = context['ti'].xcom_pull(key='current_value',task_id='subtract_three_task')
+    current_value = context['ti'].xcom_pull(key='current_value',task_ids='subtract_three_task')
     new_value = current_value ** 2
     print(f'Square: {current_value} ^ 2 = {new_value}')
 
@@ -38,10 +38,10 @@ with DAG (
     catchup = False
 ) as dag:
     
-    start_task = PythonOperator(task_id='start_task',python_callable=start_number,provide_context=True)
-    add_five_task = PythonOperator(task_id='add_five_task',python_callable=add_five,provide_context=True)
-    multiply_by_two_task = PythonOperator(task_id='multiply_by_two_task',python_callable=multiply_by_two,provide_context=True)
-    subtract_three_task = PythonOperator(task_id='subtract_three_task',python_callable=subtract_three,provide_context=True)
-    square_number_task = PythonOperator(task_id='square_number_task',python_callable=square_number,provide_context=True)
+    start_task = PythonOperator(task_id='start_task',python_callable=start_number)
+    add_five_task = PythonOperator(task_id='add_five_task',python_callable=add_five)
+    multiply_by_two_task = PythonOperator(task_id='multiply_by_two_task',python_callable=multiply_by_two)
+    subtract_three_task = PythonOperator(task_id='subtract_three_task',python_callable=subtract_three)
+    square_number_task = PythonOperator(task_id='square_number_task',python_callable=square_number)
 
     start_task >> add_five_task >> multiply_by_two_task >> subtract_three_task >> square_number_task
